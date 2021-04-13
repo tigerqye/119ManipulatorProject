f312af273d611afe26893b04a8b1459a675fd09e
Tiger Ye <bobcorpisepic@gmail.com>
2021-04-12 23:48:26 -0400

    IT WORKS

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@ using namespace std;
 
 const double WL = 5;
 const double GRAVITY = 9.81;
+const double MAX_LENGTH = 388;
 
 class Point {
 private:
@@ -36,6 +37,9 @@ public:
         return sqrt(pow(x,2) + pow(y,2));
     }
 
+    double angleOrigin()const{
+        return atan(y/x);
+    }
 };
 
 class Link {
@@ -51,7 +55,7 @@ public:
     Link(Point start0, Point end0, double width0){
         start = start0;
         end = end0;
-        length = sqrt(pow(end.getx() - start.getx(),2) + pow(end.gety() - end.gety(),2));
+        length = sqrt(pow(end.getx() - start.getx(),2) + pow(end.gety() - start.gety(),2));
         width = width0;
     };
 
@@ -68,7 +72,7 @@ public:
     }
     //radians wrt x axis
     double getAngle()const{
-        return atan((end.gety() - end.gety())/(end.getx() - start.getx()));
+        return atan((end.gety() - start.gety())/(end.getx() - start.getx()));
     }
 
     double xDist()const{
@@ -83,9 +87,54 @@ public:
 
 double getTorque(Link links[3]){
     return links[0].xDist()*links[0].getWeight()/2
-            + (links[0].xDist() + links[1].xDist()/2)*links[1].getWeight()
-            + (links[0].xDist() + links[1].xDist() + links[2].xDist()/2)*links[2].getWeight()
-            + (links[0].xDist() + links[1].xDist() + links[2].xDist())*WL*GRAVITY;
+           + (links[0].xDist() + links[1].xDist()/2)*links[1].getWeight()
+           + (links[0].xDist() + links[1].xDist() + links[2].xDist()/2)*links[2].getWeight()
+           + (links[0].xDist() + links[1].xDist() + links[2].xDist())*WL*GRAVITY;
+}
+
+double firstPosition(double l1, double l2, double l3){
+    Point end = Point(0.75,0.10);
+    Point p2 = Point(0.75 - l3*cos(M_PI/3), 0.10 + l3*sin(M_PI/3));
+    if(l1 + l2 < p2.distOrigin())
+    {
+        return 1000;
+    }
+    double angle1 = acos((pow(l2,2) - pow(l1,2) - pow(p2.distOrigin(),2))/(-2*l1*p2.distOrigin()));
+    Point p11 = Point(l1*cos(angle1+p2.angleOrigin()),l1*sin(angle1+p2.angleOrigin()));
+    Point p12 = Point(l1*cos(p2.angleOrigin()-angle1),l1*sin(p2.angleOrigin()-angle1));
+    Link links1[] = {Link(Point(0,0),p11,4), Link(p11,p2,2), Link(p2,end,1) };
+    Link links2[] = {Link(Point(0,0),p12,4), Link(p12,p2,2), Link(p2,end,1) };
+    return min(getTorque(links1),getTorque(links2));
+}
+
+double secondPosition(double l1, double l2, double l3){
+    Point end = Point(0.50,0.50);
+    Point p2 = Point(0.50 - l3, 0.50);
+    if(l1 + l2 < p2.distOrigin())
+    {
+        return 1000;
+    }
+    double angle1 = acos((pow(l2,2) - pow(l1,2) - pow(p2.distOrigin(),2))/(-2*l1*p2.distOrigin()));
+    Point p11 = Point(l1*cos(angle1+p2.angleOrigin()),l1*sin(angle1+p2.angleOrigin()));
+    Point p12 = Point(l1*cos(p2.angleOrigin()-angle1),l1*sin(p2.angleOrigin()-angle1));
+    Link links1[] = {Link(Point(0,0),p11,4), Link(p11,p2,2), Link(p2,end,1) };
+    Link links2[] = {Link(Point(0,0),p12,4), Link(p12,p2,2), Link(p2,end,1) };
+    return min(getTorque(links1),getTorque(links2));
+}
+
+double thirdPosition(double l1, double l2, double l3){
+    Point end = Point(0.20,0.60);
+    Point p2 = Point(0.20 - l3*cos(M_PI/4), 0.60 - l3*sin(M_PI/4));
+    if(l1 + l2 < p2.distOrigin())
+    {
+        return 1000;
+    }
+    double angle1 = acos((pow(l2,2) - pow(l1,2) - pow(p2.distOrigin(),2))/(-2*l1*p2.distOrigin()));
+    Point p11 = Point(l1*cos(angle1+p2.angleOrigin()),l1*sin(angle1+p2.angleOrigin()));
+    Point p12 = Point(l1*cos(p2.angleOrigin()-angle1),l1*sin(p2.angleOrigin()-angle1));
+    Link links1[] = {Link(Point(0,0),p11,4), Link(p11,p2,2), Link(p2,end,1) };
+    Link links2[] = {Link(Point(0,0),p12,4), Link(p12,p2,2), Link(p2,end,1) };
+    return min(getTorque(links1),getTorque(links2));
 }
 
 double getTotalTorque(double t1, double t2, double t3)
@@ -93,12 +142,28 @@ double getTotalTorque(double t1, double t2, double t3)
     return sqrt(pow(t1,2) + pow(t2,2) + pow(t3,2));
 }
 
+double combinations(){
+    double least = 1000;
+    for(int i = 1; i < MAX_LENGTH - 2; i++)
+    {
+        for(int k = 1; k < MAX_LENGTH - i - 1; k++)
+        {
+            int j = MAX_LENGTH - k - i;
+            double j1 = j/100.0;
+            double i1 = i/100.0;
+            double k1 = k/100.0;
+            double total_torque = getTotalTorque(firstPosition(j1,k1,i1), secondPosition(j1,k1,i1), thirdPosition(j1,k1,i1));
+            if(total_torque < least && total_torque > 0)
+            {
+                cout << j << " " << k << " " << i << endl;
+                least = total_torque;
+            }
+        }
+    }
+    return least;
+}
+
 int main() {
-    Point p0 = Point(0,0);
-    Point p1 = Point(10,0);
-    Point p2 = Point(20,0);
-    Point p3 = Point(30,0);
-    Link links[] = {Link(p0,p1,4), Link(p1,p2,2), Link(p2,p3,1)};
-    cout << getTorque(links)<< endl;
+    cout << combinations() << endl;
     return 0;
 }
